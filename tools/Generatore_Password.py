9db54c418c7b7b88decec4f08f69f432e52880f4
davidebrutto <davide@davidebrutto.it>
2026-05-27 13:33:00 +0200

    1.0.0

---
+++ tools/Generatore_Password.py
@@ -0,0 +1,8 @@
+from werkzeug.security import generate_password_hash
+
+# Sostituisci con la password che desideri impostare
+nuova_password = "BrtDeM110271@"
+
+# Genera l'hash con lo stesso algoritmo (PBKDF2 con SHA-256)
+nuovo_hash = generate_password_hash(nuova_password, method='pbkdf2:sha256', salt_length=16)
+print(nuovo_hash)
