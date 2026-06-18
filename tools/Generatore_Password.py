7b4c74f8506057dfc7ff95e83bb5d9e100a03d79
davidebrutto <davide@davidebrutto.it>
2026-06-18 13:51:00 +0200

    1.0.0
    Ripresa sviluppo

--- tools/Generatore_Password.py
+++ tools/Generatore_Password.py
@@ -1,7 +1,7 @@
 from werkzeug.security import generate_password_hash
 
 # Sostituisci con la password che desideri impostare
-nuova_password = "BrtDeM110271@"
+nuova_password = "federscherma"
 
 # Genera l'hash con lo stesso algoritmo (PBKDF2 con SHA-256)
 nuovo_hash = generate_password_hash(nuova_password, method='pbkdf2:sha256', salt_length=16)
